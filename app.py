dd78d49994768b92ed3165f491473bf08e502aa6
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-17 15:38:55 +0530

    ..

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from keras.models import  load_model
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
